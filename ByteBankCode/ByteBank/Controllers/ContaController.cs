c15c70e9188253971a4e90de6718af354655e8bb
deliasfrancisco <deliasfrancisco@gmail.com>
2019-06-06 09:52:50 -0300

    Commit para subir o projeto para o Git

---
+++ ByteBankCode/ByteBank/Controllers/ContaController.cs
@@ -0,0 +1,112 @@
+﻿using ByteBank.Models;
+using ByteBank.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ByteBank.Controllers
+{
+	public class ContaController : Controller
+	{
+		private UserManager<UsuarioAplicacao> _userManager;
+		public UserManager<UsuarioAplicacao> UserManager
+		{
+			get
+			{
+				if(_userManager == null)
+				{
+					var contextOwin = HttpContext.GetOwinContext();
+					_userManager = contextOwin.GetUserManager<UserManager<UsuarioAplicacao>>();
+				}
+				return _userManager;
+			}
+		}
+
+		public ActionResult Registrar()
+		{
+			return View();
+		}
+
+
+		[HttpPost]
+		public async Task<ActionResult> Registrar(ContaRegistrarViewModel modelo)
+		{
+			if (ModelState.IsValid)
+			{
+				var novoUsuario = new UsuarioAplicacao();
+
+				novoUsuario.Email = modelo.Email;
+				novoUsuario.UserName = modelo.UserName;
+				novoUsuario.NomeCompleto = modelo.NomeCompleto;
+
+				var usuario = await UserManager.FindByEmailAsync(modelo.Email);
+				var usuarioJaExiste = usuario != null;
+
+				if (usuarioJaExiste)
+				{
+					return View("AguardandoConfirmacao");
+				}
+
+				var resultado = await UserManager.CreateAsync(novoUsuario, modelo.Senha);
+
+				if (resultado.Succeeded)
+				{
+					await EnviarEmailDeConfirmacao(novoUsuario);
+					return View("AguardandoConfirmacao");
+				}
+				else
+				{
+					AdicionaErros(resultado);
+				}	
+				//estava sendo ignorado o resultado do create async, então foi criado um metodo para receber o resultado do Succeded que vem do UserManager.CreateAsync (true or false)
+				// Se == true ele faz o login, senão foi gerado um metodo para adicionar erros onde ele percorre uma lista de erros e encontrando o valor do erro ele retorna a mensagem 
+			}
+			return View();
+		}
+
+		private async Task EnviarEmailDeConfirmacao(UsuarioAplicacao usuario)
+		{
+			var token = await UserManager.GenerateEmailConfirmationTokenAsync(usuario.Id);
+
+			var linkDeCallback = Url.Action(
+				"ConfirmacaoEmail",
+				"Conta",
+				new { usuarioId = usuario.Id, token = token },
+				Request.Url.Scheme);
+
+			await  UserManager.SendEmailAsync(usuario.Id, "ByteBank - Email de confirmação", $"Bem vindo ao fórum ByteBank, clique aqui {linkDeCallback} para confirmar seu email!");
+		}
+
+		public async Task<ActionResult> ConfirmacaoEmail(string usuarioId, string token)
+		{
+			if (usuarioId == null || token == null)
+			{
+				return View("Error");
+			}
+
+			var resultado = await UserManager.ConfirmEmailAsync(usuarioId, token);
+
+			if (resultado.Succeeded)
+				return RedirectToAction("Index", "Home");
+
+			else
+				return View("Error");
+
+			throw new NotImplementedException();
+		}
+
+		private void AdicionaErros(IdentityResult resultado)
+		{
+			foreach (var erro in resultado.Errors)
+			{
+				ModelState.AddModelError("", erro);
+			}
+		}
+	}
+}
---
+++ ByteBankCode/ByteBank/Startup.cs
@@ -0,0 +1,66 @@
+﻿using ByteBank.App_Start.Identity;
+using ByteBank.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(ByteBank.Startup))]
+
+namespace ByteBank
+{
+
+
+	public class Startup
+	{
+
+		public void Configuration(IAppBuilder builder)
+		{
+			builder.CreatePerOwinContext<DbContext>(() =>
+				new IdentityDbContext<UsuarioAplicacao>("DefaultConnection")
+			);
+
+			builder.CreatePerOwinContext<IUserStore<UsuarioAplicacao>>(
+				(opcoes, contextOwin) =>
+				{
+					var dbContext = contextOwin.Get<DbContext>();
+					return new UserStore<UsuarioAplicacao>(dbContext);
+				});
+
+			builder.CreatePerOwinContext<UserManager<UsuarioAplicacao>>(
+				(opcoes, contextOwin) =>
+				{
+					var userStore = contextOwin.Get<IUserStore<UsuarioAplicacao>>();
+					var userManager = new UserManager<UsuarioAplicacao>(userStore);
+
+					var userValidator = new UserValidator<UsuarioAplicacao>(userManager);
+					userValidator.RequireUniqueEmail = true;
+
+					userManager.UserValidator = userValidator;
+					userManager.PasswordValidator = new SenhaValidador() {
+						TamanhoRequerido = 6,
+						ObrigatorioCaracteresEspeciais = true,
+						ObrigatorioLowerCase = true,
+						ObrigatorioUpperCase = true,
+						ObrigatorioDigitos = true
+					};
+
+					userManager.EmailService = new EmailServico();
+
+					var dataProtectionPorvider = opcoes.DataProtectionProvider;
+					var dataProtectionPorviderCreate = dataProtectionPorvider.Create("ByteBank");
+
+					userManager.UserTokenProvider = new DataProtectorTokenProvider<UsuarioAplicacao >(dataProtectionPorviderCreate);
+
+					return userManager;
+				});
+		}
+
+	}
+}
